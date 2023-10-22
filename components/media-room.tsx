2d0248e6aeb3f378cd78af910e3d7bedb4797c10
MathLike <benjamin.ubilla@uc.cl>
2023-10-22 13:51:07 -0300

    First commit

---
+++ components/media-room.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+interface MediaRoomProps {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+};
+
+export const MediaRoom = ({
+  chatId,
+  video,
+  audio
+}: MediaRoomProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (
+      typeof user?.firstName == "undefined" ||
+      typeof user?.lastName == "undefined"
+    )
+      return;
+    
+    //prettier-ignore
+    const name = `${user?.username}`;
+    // the random4DigitNumber to prevent disconnected livekit if 2 person have same name
+
+    (async () => {
+      try {
+        const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    })()
+  }, [user?.firstName, user?.lastName, chatId]);
+
+  if (token === "") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2
+          className="h-7 w-7 text-zinc-500 animate-spin my-4"
+        />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading...
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={video}
+      audio={audio}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  )
+}
